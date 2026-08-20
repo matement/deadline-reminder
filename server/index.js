d9552680d3d35c12f0aa329c0e09956c824b3b14
MATEO MENTOLLI <csd5452@csd.uoc.gr>
2026-08-20 05:10:49 +0300

    Log healthz checks

--- server/index.js
+++ server/index.js
@@ -20,11 +20,14 @@ app.use(express.json());
 app.get('/healthz', async (req, res, next) => {
   try {
     if (req.query.run === '1') {
+      console.log('[healthz] scheduler check triggered');
       const result = await checkAndSendReminders();
+      console.log('[healthz] scheduler check finished:', JSON.stringify(result));
       return res.json({ ok: true, ranScheduler: true, ...result });
     }
     res.json({ ok: true });
   } catch (err) {
+    console.error('[healthz] scheduler check threw:', err);
     next(err);
   }
 });
